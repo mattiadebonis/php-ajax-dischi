1dd0061aaefc8a8e477dc40816fece3e95aeeec5
mattiadebonis <mattia.debonis@gmail.com>
2021-06-30 16:37:35 +0200

    Database

---
+++ partial/database.php
@@ -0,0 +1,68 @@
+<?php
+    $dischi = [
+        [
+            "titolo" => "Stronger",
+            "autore" => "Kanye West", 
+            "anno" => "2009",
+            "genere" => "Hip Hop",
+            "copertina" => "",
+        ],
+        [
+            "titolo" => "Stronger",
+            "autore" => "Kanye West", 
+            "anno" => "2009",
+            "genere" => "Hip Hop",
+            "copertina" => "./img/mother-danzig.jpeg",
+        ],
+        [
+            "titolo" => "Live Your Life",
+            "autore" => "Rihanna", 
+            "anno" => "2009",
+            "genere" => "Hip Hop",
+            "copertina" => "./img/mother-danzig.jpeg",        ],
+        [
+            "titolo" => "The Downeaster 'Alexa'",
+            "autore" => "Billy Joel", 
+            "anno" => "1995",
+            "genere" => "Rock",
+            "copertina" => "./img/mother-danzig.jpeg",        ],
+        [
+            "titolo" => "Love train",
+            "autore" => "Wolfmother", 
+            "anno" => "2009",
+            "genere" => "Rock",
+            "copertina" => "./img/mother-danzig.jpeg",        ],
+        [
+            "titolo" => "Fever",
+            "autore" => "The cramps", 
+            "anno" => "1945",
+            "genere" => "Jazz",
+            "copertina" => "./img/mother-danzig.jpeg",        ],
+        [
+            "titolo" => "In The Air Tonight",
+            "autore" => "Phil Collins", 
+            "anno" => "1983",
+            "genere" => "Rock",
+            "copertina" => "./img/mother-danzig.jpeg",        ],
+        [
+            "titolo" => "One Night in Bangkok",
+            "autore" => "Myke Tyson", 
+            "anno" => "2010",
+            "genere" => "Pop",
+            "copertina" => "./img/mother-danzig.jpeg",        ],
+        [
+            "titolo" => "Mother",
+            "autore" => "Danzing", 
+            "anno" => "1976",
+            "genere" => "Rock",
+            "copertina" => "./img/mother-danzig.jpeg",        ],
+        [
+            "titolo" => "Stronger",
+            "autore" => "Kanye West", 
+            "anno" => "2009",
+            "genere" => "Hip Hop",
+            "copertina" => "./img/mother-danzig.jpeg",        ],
+    ]
+?>
+
+

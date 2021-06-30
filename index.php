db6b253be850a27c2ee32ecb7d9daf272b5abf58
mattiadebonis <mattia.debonis@gmail.com>
2021-06-30 17:15:50 +0200

    Esercizio completato

--- index.php
+++ index.php
@@ -4,22 +4,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Php dischi di Mattia De Bonis</title>
+    <!-- css -->
+    <link rel="stylesheet" href="style/style.css">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;700&display=swap" rel="stylesheet">
+    
 </head>
 <body>
+    <ul id="cards">
+        <?php 
+        include __DIR__ . "/partial/database.php";
+        
+        foreach ($dischi as $chiaveDisco => $disco) {
 
-    <?php 
-       include __DIR__ . "/partial/database.php";
-       
-       foreach ($dischi as $chiaveDisco => $disco) {
-            echo($disco["titolo"]);
-            echo($disco["autore"]);
-            echo($disco["anno"]);
-            echo($disco["genere"]);
-            echo($disco["copertina"]);
+        ?>
+        <!-- card disco -->
+        <li class="card">
+            <?php 
+                    echo("<img class='immagine-disco' src='" . $disco["copertina"]. "' />");
 
-       } 
-    ?>
+                    echo("<h2>".$disco["titolo"]."</h2>");
+                    echo("<h3>".$disco["autore"]."</h3>");
+                    echo("<h4>".$disco["anno"]."</h4>");
+                    echo("<h5>".$disco["genere"]."</h5>");
+            ?>
+        </li>
+        <!-- /card disco -->
+        <?php 
+        } 
+        ?>
+    </ul>
+    
     
 </body>
 </html>
